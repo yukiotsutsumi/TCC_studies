9b7bae418367fc85ab78c782ffe3ff1f6596a9b3
Yukio Tsutsumi <felipe.tsutsumi@gmail.com>
2023-11-22 15:40:02 -0300

    TCC do técnico, jogo 3d feito em Unity e C#

---
+++ Move_Player/Assets/Scripts/ButtonBehaviour.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var Cena: UnityEngine.SceneManagement.SceneManager;
+
+function Start () {
+
+}
+
+function Update () {
+}
+function proxTela1() {
+	Cena.LoadScene("scene");
+}
+function proxTela2(){
+	Cena.LoadScene("instrucoes");
+}
+function proxTela3(){
+	Cena.LoadScene("sobre");
+}
---
+++ Move_Player/Assets/Scripts/teste.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class teste : MonoBehaviour {
+
+    public Text scoreText;
+    public Text venceu;
+    private int score;
+
+    private void Start()
+    {
+        score = 10;
+        UpdateScore();
+    }
+	private void Update()
+	{
+
+	}
+    public void AddScore(int newScoreValue)
+    {
+        score -= newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "azul") 
+		{
+
+            if (this.gameObject.tag != "azul")
+            {
+                Debug.Log("Errou vei");
+            }
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+		}
+
+		if (other.gameObject.tag == "vermelho") 
+		{
+			if (this.gameObject.tag != "vermelho")
+				Debug.Log ("Errou vei");
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+        }
+
+		if (other.gameObject.tag == "amarelo") 
+		{
+			if (this.gameObject.tag != "amarelo")
+				Debug.Log ("Errou vei");
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+        }
+
+		if (other.gameObject.tag == "verde") 
+		{
+			if (this.gameObject.tag != "verde")
+				Debug.Log ("Errou vei");
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+        }
+
+		if (other.gameObject.tag == "marrom") 
+		{
+			if (this.gameObject.tag != "marrom")
+				Debug.Log ("Errou vei");
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+        }
+
+		if (other.gameObject.tag == "preto") 
+		{
+			if (this.gameObject.tag != "preto")
+				Debug.Log ("Errou vei");
+            else
+            {
+                Debug.Log("ACERTOU CARAI");
+            }
+        }
+	}
+}
